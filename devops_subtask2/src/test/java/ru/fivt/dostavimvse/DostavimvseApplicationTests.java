85cb0d4892f79985c52539a7b9dcb63ef0c5fd40
Dmitry Goncharov <dimanvoque@mail.ru>
2021-12-23 16:17:52 +0300

    update

--- devops_subtask2/src/test/java/ru/fivt/dostavimvse/DostavimvseApplicationTests.java
+++ devops_subtask2/src/test/java/ru/fivt/dostavimvse/DostavimvseApplicationTests.java
@@ -1,5 +1,5 @@
 //package ru.fivt.dostavimvse;
-//
+
 //import org.hibernate.Session;
 //import org.junit.Before;
 //import org.junit.Test;
@@ -9,32 +9,31 @@
 //import org.springframework.test.context.junit4.SpringRunner;
 //import ru.fivt.dostavimvse.models.Leg;
 //import ru.fivt.dostavimvse.models.Product;
-//
+
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringBootTest
 //public class DostavimvseApplicationTests {
-//
+
 //	@Test
 //	public void contextLoads() {
 //	}
-//
+
 //	Session session;
 //	@Before
 //	public void createSession() {
 //		session = HibernateSessionFactory.getSessionFactory().openSession();
 //	}
-//
+
 //	@Test
 //	public void checkProductNonNegativePriceCreated() {
-////		Leg leg = new Leg();
-//
+//		Leg leg = new Leg();
+
 //        session.beginTransaction();
 //		Product product = new Product();
 //		product.setPrice(0.5);
 //		product.setWeight(0.8);
-//
-//
-////		Integer id =
+
+
+//		Integer id =
 //	}
-//
 //}
